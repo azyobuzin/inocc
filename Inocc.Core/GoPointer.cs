85965b7995deae0e54884210ebd2362be818b689
azyobuzin <azyobuzin@users.sourceforge.jp>
2015-08-14 22:03:09 +0900

    なんでコミットしてなかったんだよ

---
+++ Inocc.Core/GoPointer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inocc.Core
+{
+    public class GoPointer<T>
+    {
+        public T Value;
+        public bool IsNil;
+
+        public void ThrowIfIsNil()
+        {
+            if (this.IsNil)
+            {
+                // runtime.errorString
+                throw new PanicException("runtime error: invalid memory address or nil pointer dereference");
+            }
+        }
+
+        public static GoPointer<T> CreateNil()
+        {
+            return new GoPointer<T>() { IsNil = true };
+        }
+    }
+}
---
+++ Inocc.Core/PanicException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Inocc.Core
+{
+    public class PanicException : Exception
+    {
+        public PanicException(object value)
+        {
+            this.Value = value;
+        }
+
+        public object Value { get; private set; }
+    }
+}
